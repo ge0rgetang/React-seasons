1ca736169b88680dccc26dd548bd460e0c6289c4
George Tang <gtang43@gmail.com>
2019-04-10 18:22:59 -0700

    Refactored functional into class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     )
+    
+//     return <div>Latitude: </div>
+// } 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// class vs functional
+// class allows structure, state (user inputs), and lifecycle
+// function is for simple stuff
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        )
+    
+      return <div>Latitude: </div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
